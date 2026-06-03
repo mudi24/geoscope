eebc0e75e11de2b34461450aeddb2d0a0108b344
yaokai <yaokai729@outlook.com>
2026-06-03 17:11:24 +0800

    fix: explicitly configure webpack alias for @ path in next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,9 +1,15 @@
+const path = require('path')
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: { unoptimized: true },
   env: {
     NEXT_PUBLIC_API_URL:
       process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000',
+  },
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.resolve(__dirname, '.')
+    return config
   },
 }
 
